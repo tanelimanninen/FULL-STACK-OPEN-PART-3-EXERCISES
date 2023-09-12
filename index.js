86d3cd4c55f206b59067131d517648730bb8f2d3
AA3977 <aa3977@student.jamk.fi>
2023-09-12 12:26:09 +0300

    Add production version frontend

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ morgan.token('req-body', (request, response) => {
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms - :req-body'))
 
 
